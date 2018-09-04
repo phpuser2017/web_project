de2a56bb60c87571f672033b99bf63ed0a7a2c63
phpuser2017 <178675994@qq.com>
2018-09-04 08:14:05 +0800

    2

--- tp5_basic/app/admin/controller/Index.php
+++ tp5_basic/app/admin/controller/Index.php
@@ -5,23 +5,10 @@ class Index extends \think\Controller
 {
     public function index()
     {
-//        dump(config());die();
         $this->assign('isassign','这里是assign分配的值');
-//        return $this->display('这里是display内容：{$isassign},还有{$name}',[
-//            'name'=>'display的第二个参数'
-//        ]);
-        return $this->fetch('index',[
-            'name'=>'里斯',
-            'email'=>'33'
-        ],['STATIC'=>'这里的是替换内容']);
-
-//        return $this->fetch('index', [
-//            'name' => 'ThinkPHP',
-//            'email' => 'thinkphp@qq.com'
-//        ]);
-//        return $this->display('index', [
-//            'name' => 'ThinkPHP',
-//            'email' => 'thinkphp@qq.com'
-//        ]);
+        $this->assign('time',time());
+        $this->assign('name','张三');
+        // $this->assign('email','178675994@qq.com');
+        return $this->fetch();
     }
 }
