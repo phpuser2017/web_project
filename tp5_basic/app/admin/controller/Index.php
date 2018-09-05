1d2a456618aafe1e4c1456194682a8e167edb19f
phpuser2017 <178675994@qq.com>
2018-09-05 18:19:58 +0800

    0905

--- tp5_basic/app/admin/controller/Index.php
+++ tp5_basic/app/admin/controller/Index.php
@@ -1,14 +1,42 @@
 <?php
 namespace app\admin\controller;
+use think\Db;
+use app\admin\model\User;
 
-class Index extends \think\Controller
+class Index
 {
     public function index()
     {
-        $this->assign('isassign','这里是assign分配的值');
-        $this->assign('time',time());
-        $this->assign('name','张三');
-        // $this->assign('email','178675994@qq.com');
-        return $this->fetch();
+//        $user=new User;
+//        $res=$user::get(2);
+//        $res=User::where('id',3)
+//            ->field('name,email')
+//            ->find()
+//            ->toArray();
+//        $res=User::get(function($query){
+//            $query->where('name','like','%张%')
+//                ->field('name,email');
+//        });
+//        $res=User::all(function($query){
+//            $query->where('id','>','1')
+//                ->field('name,email');
+//        });
+//        $res=User::all(function($query){
+//            $query->where('id','>','1')
+//                ->field('name,email');
+//        });
+//        foreach ($res as $va){
+//            $all[]=$va->toArray();
+//        }
+//        dump($all);
+        //第二个参数为数组，数组中为允许添加的字段
+        $res=User::create([
+            'name' => 'thinkphp',
+            'pass' => '12345',
+            'email' => 'thinkphp@qq.com',
+            'phone'=>'135123345678'
+        ],true);
+        dump($res->id);//获取添加的id
+        dump($res);//create方法返回的是当前模型的对象实例。
     }
 }
---
+++ tp5_basic/app/admin/model/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fan
+ * Date: 2018/09/05
+ * Time: 15:43
+ */
+namespace app\admin\model;
+use think\Model;
+
+class User extends Model
+{
+
+}
