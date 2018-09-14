7331329420fca2afd5577e89cbd18796ae3ed736
phpuser2017 <178675994@qq.com>
2018-09-14 11:06:58 +0800

    5.1

--- tp5_basic/app/admin/controller/Index.php
+++ tp5_basic/app/admin/controller/Index.php
@@ -30,13 +30,11 @@ class Index
 //        }
 //        dump($all);
         //第二个参数为数组，数组中为允许添加的字段
-        $res=User::create([
-            'name' => 'thinkphp',
-            'pass' => '12345',
-            'email' => 'thinkphp@qq.com',
-            'phone'=>'135123345678'
-        ],true);
-        dump($res->id);//获取添加的id
-        dump($res);//create方法返回的是当前模型的对象实例。
+//        $res=User::create([
+//            'name' => 'thinkphp',
+//            'pass' => '12345'
+//        ],true);
+        dump(User::where('id',3)->find()->toArray());//获取添加的id
+//        dump($res);//create方法返回的是当前模型的对象实例。
     }
 }
